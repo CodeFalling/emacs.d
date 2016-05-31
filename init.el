38aadd67375e50f1902ef3eaf01aba281b3dbdfd
codefalling <code.falling@gmail.com>
2016-05-31 21:43:59 +0800

    first commit

---
+++ init.el
@@ -0,0 +1,26 @@
+(require 'package)
+(setq package-archives '(("gnu"   . "http://elpa.zilongshanren.com/gnu/")
+			 ("org"   . "http://elpa.zilongshanren.com/org/")
+                         ("melpa" . "http://elpa.zilongshanren.com/melpa/")))
+(package-initialize)
+
+;; Bootstrap `init-loader'
+(unless (package-installed-p 'init-loader)
+  (package-refresh-contents)
+  (package-install 'init-loader))
+
+(require 'init-loader)
+(init-loader-load "~/.emacs.d/init")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (open-junk-file init-loader))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
